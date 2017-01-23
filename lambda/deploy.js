59f2f9b59ced37cb025d4cf9022b98b80eed018e
skatpgusskat <skatpgusskat@naver.com>
2017-01-23 22:53:50 +0900

    Create lambda deploy code with nodejs

---
+++ lambda/deploy.js
@@ -0,0 +1,29 @@
+var zipFolder = require('zip-folder');
+var path = require('path');
+
+var dest = path.join(__dirname, './archive.zip');
+zipFolder(__dirname, dest, function (err) {
+  if (err) {
+    console.log('Error on zipping', err);
+    return;
+  }
+  var fs = require('fs');
+
+  var zip = fs.readFileSync(dest);
+
+  var params = {
+    FunctionName: 'test',
+    Publish: true,
+    ZipFile: zip,
+  };
+
+  var AWS = require('aws-sdk');
+  var lambda = new AWS.Lambda({
+    region: 'ap-northeast-2',
+
+  });
+  lambda.updateFunctionCode(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+  });
+});
