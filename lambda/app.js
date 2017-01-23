cfe4ee27b344041abab8e54506c4a73d68b233cf
skatpgusskat <skatpgusskat@naver.com>
2017-01-23 19:29:50 +0900

    Proxy getting s3 object with auth

--- lambda/app.js
+++ lambda/app.js
@@ -4,6 +4,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors'); // TODO remove this later
 const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
+const AWS = require('aws-sdk');
+
+AWS.config.region = process.env.AWS_REGION;
+
+const bucket = new AWS.S3({
+  params: {
+    Bucket: process.env.AMAZON_S3_BUCKET_NAME,
+  },
+});
 
 const app = express();
 
@@ -21,9 +30,21 @@ app.use((req, res, next) => {
   }
 });
 
-app.get('/', (req, res) => {
-  res.json({
-    username: req.username,
+app.get('*', (req, res) => {
+  const path = req.path;
+  const pathParts = path.split('/').splice(1);
+  if (pathParts[0] !== req.username) {
+    return res.sendStatus(401);
+  }
+  const key = req.path.charAt(0) === '/' ? req.path.substr(1) : req.path;
+  return bucket.getObject({
+    Key: key,
+  })
+  .promise()
+  .then(data => res.send(data.Body))
+  .catch((err) => {
+    console.log(err);
+    res.sendStatus(404);
   });
 });
 
