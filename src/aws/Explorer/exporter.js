590bdd3ed38c3db4fa01b67a2364853e496815ff
skatpgusskat <skatpgusskat@naver.com>
2017-01-24 23:55:09 +0900

    Update explorer exporter

--- src/aws/Explorer/exporter.js
+++ src/aws/Explorer/exporter.js
@@ -1,6 +1,6 @@
+export { createObjectIfNotExists } from './createObjectIfNotExists';
+export { default as deleteObject } from './deleteObject';
 export { default as getFile } from './getFile';
 export { default as ObjectType } from './ObjectType';
-export { default as queryMyEditingFiles } from './queryMyEditingFiles';
-export { default as queryMyFiles } from './queryMyFiles';
-export { default as queryMyPostedFiles } from './queryMyPostedFiles';
+export { default as putObject } from './putObject';
 export { default as resolvePath } from './resolvePath';
