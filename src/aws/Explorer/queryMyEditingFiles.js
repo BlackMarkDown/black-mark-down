094a3764e0f5ed5f8c96ffc18609bfd556aee468
skatpgusskat <skatpgusskat@naver.com>
2017-01-29 19:56:25 +0900

    Delete unused files

--- src/aws/Explorer/queryMyEditingFiles.js
+++
@@ -1,7 +0,0 @@
-import queryMyFiles from './queryMyFiles';
-
-export default function queryMyEditingFiles() {
-  return queryMyFiles('isEditing = :true', {
-    ':true': true,
-  });
-}
--- src/aws/Explorer/queryMyPostedFiles.js
+++
@@ -1,7 +0,0 @@
-import queryMyFiles from './queryMyFiles';
-
-export default function queryMyPostedFiles() {
-  return queryMyFiles('isPublic = :true', {
-    ':true': true,
-  });
-}
