aa43b066aafb3cfaf903a5679e9548cb004d964a
skatpgusskat <skatpgusskat@naver.com>
2017-02-08 14:52:58 +0900

    Seperate fetch function by object type

--- src/aws/Explorer/getFile.js
+++ src/aws/Explorer/getFile.js
@@ -1,10 +1,16 @@
 import s3Fetch, { is2XX } from '../s3Fetch';
+import authFetch from '../authFetch';
 import resolvePath from './resolvePath';
 import ObjectType from './ObjectType';
 
 export default function getFile(path, objectType) {
   const resolvedPath = resolvePath(path, objectType);
-  return s3Fetch(resolvedPath)
+  const fetch = (
+    objectType === ObjectType.DRAFT_FILE
+    ? authFetch
+    : s3Fetch
+  );
+  return fetch(resolvedPath)
   .then((response) => {
     if (!is2XX(response)) {
       throw new Error(response.statusText);
