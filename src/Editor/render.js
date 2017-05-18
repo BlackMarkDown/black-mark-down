974b7d4088cf5f07bd6ef9b1328898b50d363949
skatpgusskat <skatpgusskat@naver.com>
2017-05-18 21:44:19 +0900

    Format other inline tags

--- src/Editor/render.js
+++ src/Editor/render.js
@@ -6,17 +6,13 @@ export default function render(textContent) {
   const markdownTree = md.parseInline(textContent);
   let rendered = '';
   markdownTree[0].children.forEach((token) => {
-    switch (token.type) {
-      case 'text': {
-        rendered += `<span md-inline="plain">${token.content}</span>`;
-      } break;
-      case 'strong_open': {
-        rendered += '<span md-inline="strong"><span class="md-meta md-before">**</span><strong>';
-      } break;
-      case 'strong_close': {
-        rendered += '</strong><span class="md-meta md-after">**</span></span>';
-      } break;
-      default: break;
+    console.log(token);
+    if (token.type === 'text') {
+      rendered += `<span md-inline="plain">${token.content}</span>`;
+    } else if (token.type.indexOf('open') > -1) {
+      rendered += `<span md-inline="${token.type}"><span class="md-meta md-before">${token.markup}</span><${token.tag}>`;
+    } else if (token.type.indexOf('close') > -1) {
+      rendered += `</${token.tag}><span class="md-meta md-after">${token.markup}</span></span>`;
     }
   });
   return rendered;
