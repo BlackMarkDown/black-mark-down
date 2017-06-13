b0b49dfa8865e7e83c1fe6561e1efbae2c6d7ed1
skatpgusskat <skatpgusskat@naver.com>
2017-06-13 14:43:46 +0900

    Refactor token expand function

--- src/Editor/expand.js
+++ src/Editor/expand.js
@@ -1,68 +1,27 @@
-import findBiggestInlineWrapper from './findBiggestInlineWrapper';
-import getNodeAndOffset from './getNodeAndOffset';
+import treeWalk from './treeWalk';
 
 function addExpandClass(node) {
-  if (!node) {
-    return;
-  }
   node.classList.add('md-expand');
 }
 
-function getNextSibilingNotText(node) {
-  let nextSibling = node.nextSibling;
-  while (nextSibling) {
-    if (nextSibling.nodeName !== '#text') {
-      return nextSibling;
-    }
-    nextSibling = nextSibling.nextSibling;
-  }
-  return null;
-}
-
-function getNextNode(node, containerElement) {
-  let nextNode = node;
-  while (nextNode && nextNode !== containerElement) {
-    const nextSibling = getNextSibilingNotText(nextNode);
-
-    if (nextSibling) {
-      return nextSibling;
-    }
-    nextNode = nextNode.parentNode;
-  }
-  return null;
-}
-
 export default function expand(containerElement, start, end) {
-  const {
-    node: startTextNode,
-    // offset: startOffset,
-  } = getNodeAndOffset(containerElement, start);
-  const {
-    node: endTextNode,
-    offset: endOffset,
-  } = getNodeAndOffset(containerElement, end);
-  const isRightDirection = start <= end;
+  const leftOffset = Math.min(start, end);
+  const rightOffset = Math.max(start, end);
 
-  const startNode = findBiggestInlineWrapper(startTextNode);
-
-  const isOnEdge = isRightDirection
-    && endTextNode
-    && endTextNode.textContent.length === endOffset;
-
-  const endNode = isOnEdge
-    ? findBiggestInlineWrapper(getNextNode(endTextNode, containerElement))
-    : findBiggestInlineWrapper(endTextNode);
-
-  addExpandClass(startNode);
-  addExpandClass(endNode);
-  if (startNode && endNode && startNode !== endNode) {
-    const leftNode = isRightDirection ? startNode : endNode;
-    const rightNode = isRightDirection ? endNode : startNode;
-
-    let nextSibling = getNextSibilingNotText(leftNode);
-    while (nextSibling !== rightNode && nextSibling) {
-      addExpandClass(nextSibling);
-      nextSibling = getNextSibilingNotText(nextSibling);
+  return treeWalk(containerElement, (node, offset) => {
+    const length = node.textContent.length;
+    const isHangingOnLeftOffset = offset <= leftOffset && leftOffset <= offset + length;
+    const isInside = leftOffset <= offset && offset <= rightOffset;
+    const isHangingOnRightOffset = offset <= rightOffset && rightOffset <= offset + length;
+    const isInlineToken = node.hasAttribute && node.hasAttribute('md-inline');
+    if (
+      isInlineToken && (
+        isHangingOnLeftOffset
+        || isInside
+        || isHangingOnRightOffset
+      )
+    ) {
+      addExpandClass(node);
     }
-  }
+  });
 }
